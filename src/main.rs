55f96e1cb8f038bcbc62314ce792ac09de68e9a7
nag763 <loic.labeye@tutanota.de>
2021-04-23 23:02:05 +0200

    Get everything in tables

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,13 @@ use std::fs;
 use clap::App;
 
 #[macro_use] extern crate clap;
+#[macro_use]
+extern crate lazy_static;
+
+lazy_static! {
+    static ref RE_TABLE_DEFS : Regex = Regex::new(r"(?i)(^\sCREATE\sTABLE[^;]*.)").unwrap();
+    static ref RE_DB_DEFS : Regex = Regex::new(r"(?i)(^\sCREATE\sDATABASE[^\n])").unwrap();
+}
 
 fn main() {
 
@@ -11,12 +18,17 @@ fn main() {
     
     if matches.is_present("FILENAME"){
 
-        
         let filename : &str = matches.value_of("FILENAME").unwrap();
-        let table_defs = Regex::new(r"(?i)(^\s*CREATe).(?i)(\s*TABLE).([^;]*)").unwrap();
         let contents = fs::read_to_string(&filename)
             .expect("Something went wrong reading the file");
-        println!("{}", table_defs.is_match(&contents));
+        let databases : Vec<&str> = RE_DB_DEFS.find_iter(&contents)
+            .map(|element| element.as_str())
+            .collect();
+        let tables : Vec<&str> = RE_TABLE_DEFS.find_iter(&contents)
+            .map(|element| element.as_str())
+            .collect();
+        println!("Databases found : {}", databases.len());
+        println!("Tables found : {}", tables.len());
     } else {
         print!("Please provide a filename. Use --help to see possibilities");
     }
