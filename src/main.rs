f458725867cc02461cab3b3feaf69f63660487d5
nag763 <loic.labeye@tutanota.de>
2021-05-01 23:27:17 +0200

    Add foreign keys

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use regex::Regex;
 use std::fs;
 use clap::App;
+use std::path::Path;
+use std::ffi::OsStr;
 
 #[macro_use] extern crate clap;
 #[macro_use] extern crate lazy_static;
@@ -9,7 +11,7 @@ lazy_static! {
     static ref RE_TABLE_DEFS : Regex = Regex::new(r"(?i)\s*CREATE\sTABLE[^;]*.").unwrap();
     static ref RE_TABLE_NAME : Regex = Regex::new(r"((?i)\s?CREATE\sTABLE\s*[`]?)+(\w*).").unwrap();
     static ref RE_FK : Regex = Regex::new(r"(?i)\sFOREIGN\sKEY").unwrap();
-    static ref RE_IN_PARENTHESES : Regex = Regex::new(r"([a-zA-Z]*)(\(([^()]+)\))").unwrap();
+    static ref RE_IN_PARENTHESES : Regex = Regex::new(r"([^`][a-zA-Z]*\s*)(\(([^()]+)\))").unwrap();
 }
 
 fn main() {
@@ -30,8 +32,12 @@ fn main() {
                 Some(value) => value,
                 _ => "output.dot",
             };
-            let mut output_content : String = String::from(init_dot(filename));
-            output_content = [output_content, tables.iter().map(|element| convert_sql_to_dot(element)).collect::<Vec<String>>().join("\n")].concat();
+            let generated_content : Vec<(String, String)> = tables.iter().map(|element| convert_sql_to_dot(element)).collect::<Vec<(String, String)>>();
+            let output_content = [
+                init_dot(filename), 
+                generated_content.iter().map(|element| element.0.as_str()).collect::<Vec<&str>>().join("\n"),
+                generated_content.iter().map(|element| element.1.as_str()).collect::<Vec<&str>>().join("\n"),
+            ].concat();
             
             match write_output_to_file(close_dot(output_content.as_str()).as_str(), output_filename) {
                 Ok(_) => println!("The output has been successfully written to the {} file", output_filename),
@@ -46,41 +52,57 @@ fn main() {
 
 }
 
-fn convert_sql_to_dot(input: &str) -> String {
+fn convert_sql_to_dot(input: &str) -> (String, String) {
     let table_name = RE_TABLE_NAME
         .captures(input)
         .unwrap()
         .get(2)
-        .map_or("TABLE NAME", |t| t.as_str().trim_start().trim_end());
-    let table_header : String = generate_table_header(table_name);
+        .map_or("TABLE NAME".to_string(), |t| trim_leading_trailing(t.as_str()));
+    let table_header : String = generate_table_header(table_name.as_str());
     let begin_dec : usize;
     let end_dec : usize;
     match input.find('('){
         Some(v) => begin_dec = v,
-        None => return close_table(table_header.as_str())
+        None => return (close_table(table_header.as_str()), "".to_string())
     }
     match input.rfind(')') {
         Some(v) => end_dec = v,
-        None => return close_table(table_header.as_str())
+        None => return (close_table(table_header.as_str()), "".to_string())
     }
-    let body_content : String = input
+    let lines : Vec<String> = input
         .chars()
         .take(end_dec)
         .skip(begin_dec+1)
         .collect::<String>()
         .split(',')
-        .map(|s| generate_attributes(s).to_string())
-        .collect::<Vec<String>>()
-        .join("\n"); 
-    
-    close_table([table_header, body_content].join("\n").as_str())
+        .map(|s| s.to_string())
+        .collect::<Vec<String>>();
+
+    let generated : Vec<(String, Option<String>)> = lines
+        .iter()
+        .map(|s| (generate_attributes(s), generate_relations(&table_name, s)))
+        .collect::<Vec<(String, Option<String>)>>();
+
+    let body_content : String = generated
+        .iter()
+        .map(|s| s.0.as_str())
+        .collect::<Vec<&str>>()
+        .join("\n");
+
+    let relations : String = generated
+        .iter()
+        .map(|s| s.1.as_deref().unwrap_or_default())
+        .collect::<Vec<&str>>()
+        .join("\n");
+
+    (close_table([table_header, body_content].join("\n").as_str()), relations)
 }
 
 fn init_dot(filename: &str) -> String {
     format!("digraph {} {{\n
     node [\n
         shape = \"plaintext\"\n
-    ]\n", filename)
+    ]\n", Path::new(filename).file_stem().unwrap_or(OsStr::new("sql")).to_str().unwrap_or("sql"))
 }
 
 fn close_dot(opened_dot: &str) -> String {
@@ -109,14 +131,14 @@ fn generate_attributes(attr: &str) -> String {
     if !attr.to_lowercase().contains("key") {
         let title : String;
         let rest : String;
-        let trimed : &str = attr.trim_start().trim_end();
+        let trimed : String = trim_leading_trailing(attr);
         if trimed.chars().collect::<Vec<char>>()[0] == '`' {
             let splitted = trimed
                 .split('`')
                 .map(|s| s.to_string())
                 .collect::<Vec<String>>();
             title = splitted[1].to_string();
-            rest = splitted[2].trim_start().to_string();
+            rest = trim_leading_trailing(splitted[2].as_str());
         } else {
             let mut splitted = trimed
                 .split(' ')
@@ -126,32 +148,58 @@ fn generate_attributes(attr: &str) -> String {
             rest = splitted.join(" ").into();
         }
         format!("<TR><TD ALIGN=\"LEFT\" BORDER=\"0\">
-                <FONT FACE=\"Roboto\">{0}</FONT>
+                <FONT FACE=\"Roboto\"><B>{0}</B></FONT>
                 </TD><TD ALIGN=\"LEFT\">
                 <FONT FACE=\"Roboto\">{1}</FONT>
-                </TD></TR>", title.trim_start().trim_end(), rest.trim_end().trim_start()
+                </TD></TR>", trim_leading_trailing(title.as_str()), trim_leading_trailing(rest.as_str())
         )
     } else {
         let is_fk : bool = RE_FK.find_iter(attr).map(|s| s.as_str()).collect::<Vec<&str>>().len() != 0;
         if is_fk {
-            let matches : Vec<&str> = RE_IN_PARENTHESES.find_iter(attr).map(|s| s.as_str()).collect();
-            let title : String = matches[0]
+            let matches : Vec<&str> = RE_IN_PARENTHESES
+                .find_iter(attr)
+                .map(
+                    |s| s.as_str()
+                ).collect::<Vec<&str>>();
+            let title : String = trim_leading_trailing(
+                matches[0]
                 .chars()
                 .take(matches[0].len()-1)
                 .skip(matches[0].find('(').unwrap()+1) 
                 .collect::<String>()
-                .trim_end()
-                .trim_start()
-                .to_string();
+                .as_str()
+                );
             format!("<TR><TD ALIGN=\"LEFT\" BORDER=\"0\">
-                <FONT FACE=\"Roboto\">[FK] {0}</FONT>
+                <FONT FACE=\"Roboto\"><B>[FK] {0}</B></FONT>
                 </TD><TD ALIGN=\"LEFT\">
                 <FONT FACE=\"Roboto\">Refers to {1}</FONT>
-                </TD></TR>", title, matches[1].trim_start().trim_end()
+                </TD></TR>", title.replace("`", ""), trim_leading_trailing(matches[1]).replace("`", "")
             )
 
         } else {
             "".to_string()
         }
     }
+}
+
+
+fn generate_relations(table_name : &str, input: &str) -> Option<String> {
+    let is_fk : bool = RE_FK.find_iter(input).map(|s| s.as_str()).collect::<Vec<&str>>().len() != 0;
+    if is_fk {
+        let replaced : &str = &input.replace("`", "");
+        let matches : Vec<&str> = RE_IN_PARENTHESES.find_iter(replaced).map(|s| s.as_str()).collect();
+        if matches.len() != 0 {
+            let table_end : &str = matches[1].split("(").collect::<Vec<&str>>()[0];
+            Some(format!("{} -> {} [label=\"{} refers {}\"]", table_name, table_end, matches[0], matches[1]))
+        } else {
+            None
+        }
+    } else {
+        None
+    }
+}
+
+
+fn trim_leading_trailing(input : &str) -> String {
+    input.trim_start().trim_end().to_string()
 }
