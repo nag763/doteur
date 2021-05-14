ecf84cfd1a8f578663ead27c395ac5327da1cf8d
nag763 <loic.labeye@tutanota.de>
2021-05-14 15:03:41 +0200

    Update build script

--- build.sh
+++ build.sh
@@ -9,9 +9,21 @@ cargo build --release --target x86_64-pc-windows-gnu #Windows x64-86
 rm -rf release
 mkdir release
 
-zip release/doteur_windows_86_64.zip target/x86_64-pc-windows-gnu/release/* -r
-zip release/doteur_linux_86_64.zip target/release/* -r
+cd target/x86_64-pc-windows-gnu/
+mv release doteur
+zip ../../release/doteur_windows_86_64.zip ./doteur -r
 
-md5sum release/doteur_windows_86_64.zip > release/doteur_windows_86_64.zip.md5
-md5sum release/doteur_linux_86_64.zip > release/doteur_linux_86_64.zip.md5
+cd ../..
 
+cd target
+mv release doteur
+zip ../release/doteur_linux_86_64.zip ./doteur -r
+
+cd ../release
+
+md5sum doteur_windows_86_64.zip > doteur_windows_86_64.zip.md5
+md5sum doteur_linux_86_64.zip > doteur_linux_86_64.zip.md5
+
+cd ..
+
+echo "Done with success"
