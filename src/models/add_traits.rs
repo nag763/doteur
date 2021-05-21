b7e01bd4806ad29adba9650cb5b61b495f771845
nag763 <loic.labeye@tutanota.de>
2021-05-21 21:41:16 +0200

    Fix tests

--- src/models/add_traits.rs
+++ src/models/add_traits.rs
@@ -32,3 +32,51 @@ impl Replacable for String {
         self.chars().filter(|c| c.is_ascii_alphanumeric() || c.is_ascii_whitespace()).collect::<String>()
     }
 }
+
+
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn trim_trailing() {
+        assert_eq!(" hello".trim_leading_trailing(), "hello");
+        assert_eq!(String::from(" hello").trim_leading_trailing(), String::from("hello"));
+    }
+
+    #[test]
+    fn trim_leading() {
+        assert_eq!("hello ".trim_leading_trailing(), "hello");
+        assert_eq!(String::from("hello ").trim_leading_trailing(), String::from("hello"));
+    }
+
+    #[test]
+    fn trim_leading_trailing() {
+        assert_eq!("  hello ".trim_leading_trailing(), "hello");
+        assert_eq!(String::from("  hello ").trim_leading_trailing(), String::from("hello"));
+    }
+
+    #[test]
+    fn dont_trim_in() {
+        assert_eq!("  he llo ".trim_leading_trailing(), "he llo");
+        assert_eq!(String::from("  he llo ").trim_leading_trailing(), String::from("he llo"));
+    }
+
+    #[test]
+    fn trim_other_seqs() {
+        assert_eq!(" \n\t\n he llo \t\n".trim_leading_trailing(), "he llo");
+        assert_eq!(String::from(" \t\n\n he llo \t\n").trim_leading_trailing(), String::from("he llo"));
+    }
+
+    #[test]
+    fn replace_specials() {
+        assert_eq!("h*ù$$âe🔎,;:!)l&²l<o".replace_specials(), "hello");
+    }
+
+    #[test]
+    fn replace_specials_preserves_whites_spaces() {
+        assert_eq!("\n\th ell o\t\n".replace_specials(), "\n\th ell o\t\n");
+    }
+}
