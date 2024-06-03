2777a897a2ef950a8d1d8deeb53664b751a60151
Loïc <55486724+nag763@users.noreply.github.com>
2024-06-03 22:14:42 +0200

    Start impl

--- doteur_wasm/push_to_pages.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-wasm-pack build --target web --no-typescript --out-dir example/pkg --release
-cp example/* ../../doteur-pages/docs_src/live -r
---
+++ doteur_wasm/src/main.rs
@@ -0,0 +1,7 @@
+use leptos::{mount_to_body, view};
+
+fn main() {
+    mount_to_body(|| view!{
+        <p>{"Hello"}</p>
+    });
+}
